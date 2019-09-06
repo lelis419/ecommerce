52d6ef5a4bd707626415326b3de6948096dc58fd
Leonardo <emaildeteste308@gmail.com>
2019-09-06 11:18:19 -0300

    CRUD

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,11 @@ use \Hcode\Model;
 class User extends Model{
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_secret";
+
+	protected $fields = [
+		"iduser", "idperson", "desperson", "nrphone", "desemail", "deslogin", "despassword", "inadmin", "dtergister"
+	];
 
 	public static function login($login, $password){
 
@@ -65,6 +70,80 @@ class User extends Model{
 	public function logout() {
 
 		$_SESSION[User::SESSION] = NULL;
+
+	}
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+
+		$this->getData($results);
+
+	}
+
+	public function get($iduser)
+	{
+	 
+	 $sql = new Sql();
+	 
+	 $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+	 ":iduser"=>$iduser
+	 ));
+	 
+	 $data = $results[0];
+	 
+	 $this->setData($data);
+	 
+	}
+
+	public function update()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+
+		$this->getData($results);
+
+	}
+
+	public function delete()
+	{
+
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
 
 	}
 
